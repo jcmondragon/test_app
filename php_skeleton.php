a5c2e452b4a582b4dfa4805e8c37fa1f4dddec13
Julio César Mondragón Álvarez <jcmondragon@validata.com.mx>
2015-03-04 19:10:58 -0600

    Cambios primeros

--- php_skeleton.php
+++ php_skeleton.php
@@ -1,17 +1,12 @@
 <?php
 	session_start();
-	$author_operator_id = $_SESSION['operator_id'];
-	require_once("common.php");
-	require_once("menu_item.php");
 	header("Content-type: text/html");
 	// Connect to database.
-	$dbh = pdo_db_connect();
-	pdo_validate_program($dbh,1);
 
 // SET NEW FUNCTION CALLS HERE
 
 	if( isset($_REQUEST['first']) )
-		forma($dbh);
+		forma();
 
 	$dbh = NULL;
 	
@@ -19,7 +14,6 @@
 		echo <<<end_html
 			<head>
 				<title></title>
-				<link rel = 'stylesheet' type = 'text/css' href = 'maestro.css'></link>
 				<script type = 'text/javascript' src = 'jquery.js'></script>
 				<script type = 'text/javascript'>
 				</script>
@@ -31,7 +25,7 @@ end_html;
 
 // SET NEW FUNCTIONS HERRE
 
-	function forma($dbh){
+	function forma(){
 		basic_header();
 	}
 
